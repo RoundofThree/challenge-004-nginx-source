5f3245a3a9432d6485638d6f2b302a56967661f0
RoundofThree <zhuoyingjiangli@gmail.com>
2025-01-19 17:43:54 +0000

    [CHERI] Increase default sizes of types_hash_bucket_size and variables_hash_bucket_size to 128

--- src/http/ngx_http_core_module.c
+++ src/http/ngx_http_core_module.c
@@ -3419,7 +3419,7 @@ ngx_http_core_init_main_conf(ngx_conf_t *cf, void *conf)
 
 
     ngx_conf_init_uint_value(cmcf->variables_hash_max_size, 1024);
-    ngx_conf_init_uint_value(cmcf->variables_hash_bucket_size, 64);
+    ngx_conf_init_uint_value(cmcf->variables_hash_bucket_size, 128);
 
     cmcf->variables_hash_bucket_size =
                ngx_align(cmcf->variables_hash_bucket_size, ngx_cacheline_size);
