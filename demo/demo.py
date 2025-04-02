abfc0ec90ff5e565a3d8de90ce38953992ffc87e
RoundofThree <zhuoyingjiangli@gmail.com>
2025-04-02 01:59:43 +0100

    [demo] Run triggers concurrently

--- demo/demo.py
+++ demo/demo.py
@@ -8,14 +8,31 @@ import signal
 import re
 import subprocess
 from tabulate import tabulate
+import concurrent.futures
 
 # ugly sleep times too
 # hardcoded
-conf_file = "demo.conf"
+conf_file = {
+    'aarch64': 'demo-aarch64.conf',
+    'aarch64c': 'demo-aarch64c.conf',
+    'aarch64c+Q0': 'demo-aarch64c-q0.conf',
+}
 host = "127.0.0.1"
-http_port = 8080
-smtp_port = 2525
-pop3_port = 1111
+http_port = {
+    'aarch64': 8080,
+    'aarch64c': 8081,
+    'aarch64c+Q0': 8082,
+}
+smtp_port = {
+    'aarch64': 2525,
+    'aarch64c': 2526,
+    'aarch64c+Q0': 2527,
+}
+pop3_port = {
+    'aarch64': 1111,
+    'aarch64c': 1112,
+    'aarch64c+Q0': 1113,
+}
 
 # 3 configurations: aarch64, aarch64c, aarch64c+Q0
 configurations = ["aarch64", "aarch64c", "aarch64c+Q0"]
@@ -23,6 +40,9 @@ nginx_aarch64c_bin = "../install_aarch64c/sbin/nginx"
 nginx_aarch64_bin = "../install_aarch64/sbin/nginx"
 aarch64_error_log_path = '/tmp/nginx_aarch64/error.log'
 aarch64c_error_log_path = '/tmp/nginx_aarch64c/error.log'
+aarch64_pid_path = '/tmp/nginx_aarch64/nginx.pid'
+aarch64c_pid_path = '/tmp/nginx_aarch64c/nginx.pid'
+aarch64c_q0_pid_path = '/tmp/nginx_aarch64c/nginx-q0.pid'
 
 # results. cpv number (where bcpv counts as 17+i) -> PID
 aarch64_pids = {}
@@ -32,21 +52,23 @@ aarch64c_q0_pids = {}
 table_headers = ["CPV", "aarch64", "aarch64c", "aarch64c+Q0"]
 table_rows = []
 
-def get_nginx_worker_pid():
-    try:
-        output = subprocess.check_output(["ps", "aux"]).decode('utf-8')
-    except subprocess.CalledProcessError as e:
-        print("Error executing ps aux:", e)
-        return None
 
-    for line in output.splitlines():
-        # very hackish and prone to error
-        if "nginx: worker" in line:
-            # Split the line into columns (the PID is the second column)
-            columns = line.split()
-            if len(columns) >= 2:
-                return int(columns[1])
-    return None
+def get_nginx_worker_pid(configuration):
+    master_pid = None
+    if configuration == "aarch64":
+        pid_path = aarch64_pid_path
+    elif configuration == "aarch64c":
+        pid_path = aarch64c_pid_path
+    else:
+        pid_path = aarch64c_q0_pid_path
+    try:
+        with open(pid_path, 'r') as fp:
+            master_pid = int(fp.read().strip())
+    except:
+        print("Error reading pid file", pid_path)
+        return None
+    worker_pid = int(subprocess.check_output(["pgrep", "-P", str(master_pid)]).decode().split()[0])
+    return worker_pid
 
 def start_nginx(config_file, configuration):
     absolute_config_path = os.path.abspath(config_file)
@@ -67,23 +89,25 @@ def start_nginx(config_file, configuration):
 def stop_nginx(configuration):
     if configuration == "aarch64":
         os.system(f"{nginx_aarch64_bin} -s stop")
-    else:
+    elif configuration == "aarch64c":
         os.system(f"{nginx_aarch64c_bin} -s stop")
+    else: # aarch64c+Q0
+        os.system(f"kill $(cat {aarch64c_q0_pid_path})")
     # print("[+] NGINX stopped")
 
 
-def send_request(request, verbose=False):
+def send_request(request, configuration, verbose=False):
     if not request:
         raise RuntimeError("[!] Error: Request file is empty.")
     
     port = 0
     # rather simplistic and overfit logic
     if b'HTTP' in request:
-        port = http_port
+        port = http_port[configuration]
     elif b'USER' in request:
-        port = pop3_port
+        port = pop3_port[configuration]
     else:
-        port = smtp_port
+        port = smtp_port[configuration]
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         try:
@@ -101,11 +125,36 @@ def list_blob_files(directory="."):
     return [f for f in os.listdir(directory) if f.endswith('.blob') and os.path.isfile(os.path.join(directory, f))]
 
 
+def run_trigger(configuration, cpv_number, request):
+    worker_pid = -1
+    prev_pid = -1
+    try:
+        print(f'[*] Configuration {configuration}')
+        start_nginx(conf_file[configuration], configuration)
+        # time.sleep(2)
+        prev_pid = get_nginx_worker_pid(configuration)
+        print(f'[*] Nginx worker PID = {prev_pid}')
+        # save the pid
+        if configuration == "aarch64":
+            aarch64_pids[cpv_number] = prev_pid
+        elif configuration == "aarch64c":
+            aarch64c_pids[cpv_number] = prev_pid
+        else:
+            aarch64c_q0_pids[cpv_number] = prev_pid
+        send_request(request, configuration, verbose=False)
+        time.sleep(1) # allow some time
+        worker_pid = get_nginx_worker_pid(configuration)
+        if worker_pid != prev_pid:
+            print(f'[+] Nginx worker process crashed!')
+        else:
+            print(f"[!] Nginx worker process didn't crash")
+    finally:
+        stop_nginx(configuration)
+        time.sleep(1)
+
 # run in three configurations
 def run_triggers():
     blob_files = list_blob_files()
-    worker_pid = -1
-    prev_pid = -1
     for blob_file in blob_files:
         cpv_number = int(blob_file.split('cpv')[1].split('.blob')[0])
         if 'bcpv' in blob_file:
@@ -119,30 +168,11 @@ def run_triggers():
         
         print(f'[+] Running trigger {blob_file} ' + 20 * '=')
 
-        for configuration in configurations:
-            try:
-                print(f'[*] Configuration {configuration}')
-                start_nginx(conf_file, configuration)
-                time.sleep(2)
-                prev_pid = get_nginx_worker_pid()
-                print(f'[*] Nginx worker PID = {prev_pid}')
-                # save the pid
-                if configuration == "aarch64":
-                    aarch64_pids[cpv_number] = prev_pid
-                elif configuration == "aarch64c":
-                    aarch64c_pids[cpv_number] = prev_pid
-                else:
-                    aarch64c_q0_pids[cpv_number] = prev_pid
-                send_request(request, verbose=False)
-                time.sleep(1) # allow some time
-                worker_pid = get_nginx_worker_pid()
-                if worker_pid != prev_pid:
-                    print(f'[+] Nginx worker process crashed!')
-                else:
-                    print(f"[!] Nginx worker process didn't crash")
-            finally:
-                stop_nginx(configuration)
-                time.sleep(1)
+        # run triggers in all configurations in parallel
+        with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+            futures = [executor.submit(run_trigger, configuration, cpv_number, request) for configuration in configurations]
+            for future in concurrent.futures.as_completed(futures):
+                result = future.result()
 
 
 def find_exit_signal(log_file, pid):
